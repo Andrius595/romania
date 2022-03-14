316bb18bd9841b868a9c9a9b1b103247f757374d
IgnasLeg <ignas.legota@gmail.com>
2022-03-14 14:15:49 +0200

    Added log in and log out features for users. Users are now able to log in with their email and password. Upon logging in, users are assigned a token. Users cannot log in with bad credentials. Upon logging out, users' tokens are deleted.

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,9 +18,11 @@ use Illuminate\Support\Facades\Route;
 //Public routes
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
 //Protected routes
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::post('/logout', [AuthController::class, 'logout']);
 });
