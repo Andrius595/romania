e180c05f891b607c89ba41f256d85fe908c4d49d
IgnasLeg <ignas.legota@gmail.com>
2022-04-06 18:38:02 +0300

    Created faculty and module tables. Added module search, show, create and update functions.

---
+++ api/app/Http/Controllers/ModuleController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Module;
+use PhpParser\Node\Expr\AssignOp\Mod;
+
+class ModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Module::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'faculty' => 'required',
+            'lecturer' => 'required'
+        ]);
+
+        return Module::create($request -> all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $module = Module::where('id', $id)->first();
+        if(!$module){
+            return response([
+                'message' => 'Module does not exist'
+            ], 234);
+        }
+        return Module::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $module = Module::find($id);
+        $module->update($request->all());
+        return $module;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Module::destroy($id);
+    }/**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Module::where('name', 'like', '%'.$name.'%')->get();
+    }
+}
